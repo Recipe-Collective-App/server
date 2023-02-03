aa95287161980e36e5beed2a11326aec8d1ba1b6
Sweta Shah <swetashah.8430@gmail.com>
2023-02-03 15:13:58 +0000

    local host changes to PORT

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -2,8 +2,9 @@ import upload from "../middleware/upload.js";
 import express from "express";
 export const router = express.Router();
 
+const PORT = process.env.PORT || 4001;
 router.post("/upload", upload.single("file"), async (req, res) => {
   if (req.file === undefined) return res.send("You must select a file.");
-  const imgUrl = `http://localhost:${process.env.PORT}/getImage/${req.file.filename}`;
+  const imgUrl = `http://${PORT}/getImage/${req.file.filename}`;
   return res.send(imgUrl);
 });
