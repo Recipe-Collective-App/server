62672d01c7b7129fe9ec36739ad97296668c33da
Sweta Shah <swetashah.8430@gmail.com>
2023-02-03 15:23:52 +0000

    Changes due to deplyoment

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -5,6 +5,6 @@ export const router = express.Router();
 const PORT = process.env.PORT || 4001;
 router.post("/upload", upload.single("file"), async (req, res) => {
   if (req.file === undefined) return res.send("You must select a file.");
-  const imgUrl = `${PORT}/getImage/${req.file.filename}`;
+  const imgUrl = `https://recipe-page-7duf.onrender.com/getImage/${req.file.filename}`;
   return res.send(imgUrl);
 });
